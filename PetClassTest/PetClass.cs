0af5cf3d3ebc2ea64f63ec70b2496f1efe2a7eb6
Eric Fletcher <fletcher.e@gmail.com>
2016-01-10 17:30:05 -1000

    First Commit

---
+++ PetClassTest/PetClass.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetClassTest
+{
+    public class PetClass
+    {
+        public string name { get; set; }
+        public string petType { get; set; }
+        
+        public PetClass(string n)
+        {
+            this.name = n;
+        }
+
+        public virtual void Speak()
+        {
+            Console.WriteLine("Speak");
+        }
+    }
+
+    public class PetCat : PetClass
+    {
+        public string color { get; set; }
+
+        public PetCat(string na) : base(na)
+        {
+            this.name = na;
+            this.petType = "cat";
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine("Meow");
+        }
+
+        public void SetColor(string color)
+        {
+            this.color = color;
+            GetColor();
+        }
+
+        public void GetColor()
+        {
+            Console.WriteLine("This cat's fur is now", color);
+        }
+    }
+
+    public class CoolCat : PetCat
+    {
+        public CoolCat(string name) : base(name) 
+        {
+            this.name = name;
+        }
+
+        public void sing()
+        {
+            Console.WriteLine("Haha, no.");
+        }
+
+    }
+
+    public class PetDog : PetClass
+    {
+        public PetDog (string name) : base(name)
+        {
+            this.name = name;
+            this.petType = "dog";
+        }
+
+        public override void Speak()
+        {
+            Console.WriteLine("Woof");
+        }
+    }
+}
---
+++ PetClassTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetClassTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PetClass> PetList = new List<PetClass>();
+
+            PetList.Add(new PetCat("Mittens"));
+            PetList.Add(new CoolCat("Garfield"));
+            PetList.Add(new PetDog("Bowser"));
+
+            foreach(var pet in PetList)
+            {
+                pet.Speak();
+                if(pet.petType == "cat")
+                {
+                    Console.WriteLine("Jon loves his best friend {0}", pet.name);
+
+                }
+            }
+
+            Console.WriteLine("Hello World");
+            //.ReadLine();
+        }
+    }
+}
